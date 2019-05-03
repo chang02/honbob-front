8e2030732bb3238e1febe230f68e37c3374577d7
changyoung <changyoung6789@gmail.com>
2019-05-03 14:14:29 +0900

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+  <v-layout aling-center justify-center>
+    <v-card id="test">
+      hello
+    </v-card>
+  </v-layout>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
+// import HelloWorld from '../components/HelloWorld'
 
 export default {
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
+<style>
+#test{
+  width: 100px;
+  height: 100px;
+}
+</style>
