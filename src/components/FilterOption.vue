786b7141130a941b236e62578de2ca94c03df4ea
chang02 <chang0022@snu.ac.kr>
2019-05-04 23:35:25 +0900

    filterOption

--- src/components/FilterOption.vue
+++ src/components/FilterOption.vue
@@ -2,36 +2,32 @@
     <div id="filter-option">
         <v-container fluid>
             <v-layout column>
-                <v-layout row wrap align-center v-for="i in 2" :key="i">
-                    <v-flex xs1>
-                        <v-subheader v-text="'Gender'"></v-subheader>
-                    </v-flex>
-                    <v-flex xs5>
-                        <v-select v-model="matchingGenders" :items="genders" label="Select" multiple>
-                        </v-select>
-                    </v-flex>
-                    <v-flex xs1>
-                        <v-subheader v-text="'Location'"></v-subheader>
-                    </v-flex>
-                    <v-flex xs5>
-                        <v-select v-model="targetLocations" :items="locations" label="Select" multiple>
-                        </v-select>
-                    </v-flex>
-                </v-layout>
-                <v-layout row wrap align-center>
-                    <v-flex>
-                        <v-text-field label="Search" placeholder="Keywords"></v-text-field>
-                    </v-flex>
-                    <v-btn flat>
-                        Search
-                    </v-btn>
-                    <v-flex>
-                        <v-text-field label="Search" placeholder="Keywords"></v-text-field>
-                    </v-flex>
-                    <v-btn flat>
-                        Search
-                    </v-btn>
-                </v-layout>  
+                <v-flex xs12>
+                    <v-layout row wrap align-center>
+                        <v-flex xs1>
+                            <v-subheader v-text="'Gender'"></v-subheader>
+                        </v-flex>
+                        <v-flex xs5>
+                            <v-select v-model="matchingGenders" :items="genders" label="Select" multiple>
+                            </v-select>
+                        </v-flex>
+                        <v-flex xs1>
+                            <v-subheader v-text="'Location'"></v-subheader>
+                        </v-flex>
+                        <v-flex xs5>
+                            <v-select v-model="targetLocations" :items="locations" label="Select" multiple>
+                            </v-select>
+                        </v-flex>
+                        <v-flex xs5>
+                            <v-text-field label="Search" placeholder="Keywords"></v-text-field>
+                        </v-flex>
+                        <v-flex xs1>
+                            <v-btn flat>
+                                Search
+                            </v-btn>
+                        </v-flex>
+                    </v-layout>  
+                </v-flex>
             </v-layout>
         </v-container>
     </div>
